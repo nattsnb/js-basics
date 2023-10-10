cd788b7f6a4bd7f5143d0924ceb104d1499349ef
Natasza ASzatan <natiye.nb@gmai.com>
2023-10-10 12:23:46 +0100

    added function getCricleArea

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const radius = 5;
 const area = getCircleArea(radius);
 console.log(area); // 78.53981633974483
+
+function getCircleArea(providedNumber) {
+    return Math.PI * providedNumber * providedNumber;
+}
