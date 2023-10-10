34b2cf839fa5992c0c6bf0d332f5d315cc3fb0f6
Natasza ASzatan <natiye.nb@gmai.com>
2023-10-10 12:09:41 +0100

    added function isThisBoolean

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 isThisBoolean(true);  //true
 isThisBoolean(false);  // true
 isThisBoolean('true'); // false
+
+
+function isThisBoolean(providedData) {
+    if ( providedData === false ) {
+        result = true
+    } else {
+        if ( providedData === true ) {
+            result = true
+        } else {
+            result = false}
+    }
+    console.log(result)
+}
