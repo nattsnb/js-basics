80afb772069868db2f8403815e86ff5dc1fee63b
Natasza ASzatan <natiye.nb@gmai.com>
2023-10-10 11:59:38 +0100

    added function isThisMyName

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 isThisMyName('Adam');
+
+
+function isThisMyName(firstName) {
+    if (firstName === "Natasza") {
+        result = true;
+    } else {
+        result = false;
+    }
+    console.log(result);
+    }
