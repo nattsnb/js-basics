d859aa230ce9adc261badfd2c3cc0b2b8bb7fa63
Natasza ASzatan <natiye.nb@gmai.com>
2023-10-12 13:20:02 +0100

    changed variable name

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ const radius = 5;
 const area = getCircleArea(radius);
 console.log(area); // 78.53981633974483
 
-function getCircleArea(providedNumber) {
-    return Math.PI * providedNumber * providedNumber;
+function getCircleArea(circleRadius) {
+    return Math.PI * circleRadius * circleRadius;
 }
