f085f7b78d11723196f286f3c910567de9f3e03f
Natasza ASzatan <natiye.nb@gmai.com>
2023-10-13 16:52:39 +0100

    review

--- script.js
+++ script.js
@@ -5,24 +5,24 @@ rockPaperScissors('paper','paper'); // 0
 
 function rockPaperScissors(playerOne, playerTwo) {
     if (playerOne === playerTwo) {
-        console.log("0")
+        return 0
     } else if (playerOne === "rock") {
         if (playerTwo === "paper") {
-            console.log("2")
+            return 2
         } else if (playerTwo === "scissors") {
-            console.log("1")
+            return 1
         }
     } else if (playerOne === "scissors") {
         if (playerTwo === "paper") {
-            console.log("1")
+            return 1
         } else if (playerTwo === "rock") {
-            console.log("2")
+            return 2
         }
     } else if (playerOne === "paper") {
         if (playerTwo === "rock") {
-            console.log("1")
+            return 1
         } else if (playerTwo === "scissors") {
-            console.log("2")
+            return 2
         }
     }
 }
