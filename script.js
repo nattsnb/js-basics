46c6f33d149bb216da5740206096f9378ca52b0c
Natasza ASzatan <natiye.nb@gmai.com>
2023-10-13 17:05:03 +0100

    review

--- script.js
+++ script.js
@@ -3,5 +3,6 @@ swapNumber( 5, 2)
 swapNumber( 100, 300)
 
 function swapNumber(numberOne, numberTwo) {
-    console.log(numberTwo, numberOne)
+    newVariable = [numberTwo, numberOne]
+    return newVariable
 }
